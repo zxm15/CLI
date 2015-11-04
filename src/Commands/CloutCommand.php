d0b06a52b7d9a715e5cb038538401e683e86026c
zxm15 <gavingan9e@gmail.com>
2015-11-04 14:05:42 -0500

    Write the clout command file

---
+++ src/Commands/CloutCommand.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zxm
+ * Date: 11/4/15
+ * Time: 2:04 PM
+ */
+
+namespace Clout\Commands;
+
+use Clout\Users\UserService;
+
+class CloutCommand extends UserServiceCommand
+{
+    const NAME = 'clout';
+    const PATTERN = '/^clout\s*/i';
+
+    public function execute(UserService $userService) {
+        if (count($this->arguments) == 2) {
+            if (strlen($this->arguments[1]) > 0) {
+                echo "Clout user ".$this->arguments[1]." \n";
+                $user = $this->arguments[1];
+                $influence = $userService->getUserInfluence($user);
+                echo $user." has ".$influence." followers \n";
+
+            } else {
+                echo "Clout all users \n";
+                $influenceArray = $userService->getInfluenceOfAllUsers();
+                foreach ($influenceArray as $user => $influence) {
+                    echo $user." has ".$influence." followers \n";
+                }
+            }
+
+        } else {
+            echo "Please write the clout command in correct form. e.g. \n";
+            echo "Get influence of a single user: clout name     e.g. clout Messi \n";
+            echo "Get influence of all users: clout     e.g. clout \n";
+        }
+    }
+}
