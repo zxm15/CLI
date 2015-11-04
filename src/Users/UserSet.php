790779ffdac5bd6843ccf02b041423975cba8103
zxm15 <gavingan9e@gmail.com>
2015-11-04 16:25:09 -0500

    Added comments for user set file

--- src/Users/UserSet.php
+++ src/Users/UserSet.php
@@ -13,17 +13,34 @@ class UserSet
 {
     private $bigFatherMap = array();
 
+    /**
+     * @param $userName
+     * @return bool
+     */
     public function contains($userName) {
         return array_key_exists($userName, $this->bigFatherMap);
     }
 
+    /**
+     * @param $userName
+     */
     public function addUser($userName) {
         $this->setBigFather($userName, $userName);
     }
 
+    /**
+     * @param $userName
+     * @param $bigFatherName
+     */
     public function setBigFather($userName, $bigFatherName) {
         $this->bigFatherMap[$userName] = $bigFatherName;
     }
+
+    /**
+     * Search for the big father and update users big father in the path
+     * @param $userName
+     * @return null
+     */
     public function compressFindBigFather($userName) {
         if (! $this->contains($userName)) return null;
         $fatherName = $this->bigFatherMap[$userName];
@@ -34,6 +51,11 @@ class UserSet
         return $bigFatherName;
     }
 
+    /**
+     * Union two sets connected with two users
+     * @param $followerName
+     * @param $followeeName
+     */
     public function unionSets($followerName, $followeeName) {
         $bigFatherOfFollower = $this->compressFindBigFather($followerName);
         $bigFatherOfFollowee = $this->compressFindBigFather($followeeName);
